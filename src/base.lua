1f74f69e7ff160c62a047787f9f758bad0a6d2f6
Colin Hall-Coates <yo@oka.io>
2016-07-08 19:30:50 -0300

    initial commit

---
+++ src/base.lua
@@ -0,0 +1,29 @@
+local Base = { fn = {} }
+
+function new (self, ...)
+    local instance = { __index = self.fn }
+
+    setmetatable(instance, instance)
+    self.__initializer(self.__source, instance, ...)
+
+    return instance
+end
+
+function Base:derive (initializer)
+    local Derivative = {
+	__call = new,
+	__index = self,
+	__initializer = initializer,
+	fn = { __index = self.fn }
+    }
+
+    function Derivative.__source (instance, ...)
+	self.__initializer(self.__source, instance, ...)
+    end
+
+    setmetatable(Derivative.fn, Derivative.fn)
+
+    return setmetatable(Derivative, Derivative)
+end
+
+return Base
